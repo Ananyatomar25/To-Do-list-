25ce275f10021b76bdf296d97016dba854729b31
Ananya Tomar <ananyatomar205@gmail.com>
2021-08-31 23:28:42 +0530

    fixed bugs

--- app.js
+++ app.js
@@ -130,7 +130,7 @@ app.get("/about",function(req,res){
 });
 
 let port = process.env.PORT;
-if(port===NULL || port ===""){
+if(port== null|| port == ""){
   port =3000;
 }
 app.listen(port, function() {
